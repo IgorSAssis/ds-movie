5229e1f6f2f501248dd2d29182e69cea81fb0c59
IgorIA <igorsemphoski@gmail.com>
2022-01-15 23:37:06 -0300

    Show score

--- front-end/src/components/MovieStars/index.tsx
+++ front-end/src/components/MovieStars/index.tsx
@@ -1,17 +1,52 @@
 import "./styles.css";
 
-import {ReactComponent as StarFull} from "assets/full-star.svg";
-import {ReactComponent as StarHalf} from "assets/half-star.svg";
-import {ReactComponent as StarEmpty} from "assets/empty-star.svg";
+import { ReactComponent as StarFull } from "assets/full-star.svg";
+import { ReactComponent as StarHalf } from "assets/half-star.svg";
+import { ReactComponent as StarEmpty } from "assets/empty-star.svg";
 
-export function MovieStars() {
+type StarProps = {
+  fill: number;
+};
+
+function Star({ fill }: StarProps) {
+  if (fill === 0) {
+    return <StarEmpty />;
+  } else if (fill === 1) {
+    return <StarFull />;
+  } else {
+    return <StarHalf />;
+  }
+}
+
+function getFills(score: number) {
+  const fills = [0, 0, 0, 0, 0];
+
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+type Props = {
+  score: number;
+};
+
+export function MovieStars({ score }: Props) {
+  const fills = getFills(score);
+
   return (
     <div className="dsmovie-stars-container">
-      <StarFull />
-      <StarFull />
-      <StarFull />
-      <StarHalf />
-      <StarEmpty />
+      {fills.map((value) => {
+        return <Star fill={value} />;
+      })}
     </div>
   );
 }
