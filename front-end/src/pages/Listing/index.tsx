2cd3ef34490646bd5ee80d3c9c2159779d445a64
IgorIA <igorsemphoski@gmail.com>
2022-01-15 23:25:53 -0300

    useParams

--- front-end/src/pages/Listing/index.tsx
+++ front-end/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ export function Listing() {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title `)
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
       .then((response) => {
         const data = response.data as MoviePage;
         setPage(data);
